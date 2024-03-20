63f02c89df56d995bdf271a4a7f6ab0407030f19
Kai <kolkowicz11@gmail.com>
2024-03-20 10:14:22 +0100

    hilfe

--- app/src/main/java/de/doomtoaster/App.java
+++ app/src/main/java/de/doomtoaster/App.java
@@ -7,6 +7,7 @@ import java.awt.Rectangle;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 import de.doomtoaster.Leiter;
@@ -15,23 +16,25 @@ public class App extends JFrame{
 	
 	public App(){
 		super();
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setSize(1920,1000);
-		setLayout(new GridLayout(3,3));		
-		setBackground(Color.red);
-		setVisible(true);
+		setTitle("My Gui");
+        setSize(400, 400);
+
+        // Create JButton and JPanel
+        JButton button = new JButton("Click here!");
+        JPanel panel = new JPanel();
+
+        // Add button to JPanel
+        panel.add(button);
+        // And JPanel needs to be added to the JFrame itself!
+        this.add(panel);
+
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 	}
 	public static void main(String[] args) {
 		App app = new App();
-		Leiter leiter = new Leiter();	
-		app.add(leiter);
-		JButton buttontest = new JButton("test");
-		app.add(buttontest);
-		
-		app.setVisible(true);
-
-
-	}
+		}
 }
 
 
--- app/src/main/java/de/doomtoaster/Leiter.java
+++ app/src/main/java/de/doomtoaster/Leiter.java
@@ -11,17 +11,18 @@ public class Leiter extends JPanel{
 	private int[] werte = {0,15,30,60,120,240,300,400,800,1200,2000,3200,5200,8400,14000};
 	public Leiter() {
 		super();
+		Sprosse testsprosse = new Sprosse(999);
 		setLayout(new GridLayout(1,werte.length));
 		setSize(100,100);
 		assemble();
-		setBackground(Color.pink);
+		setBackground(Color.pink);	
+		add(testsprosse);	
 		setVisible(true);	
-		System.out.println("atzederwo");
 	}
 	public void assemble(){
 
 		for (int i : werte) {
-			add(new Sprosse(i));
+			this.add(new Sprosse(i));
 		}
 	}
 	
